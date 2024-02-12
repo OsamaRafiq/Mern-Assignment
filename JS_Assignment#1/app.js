6352e91652574d470d7a2675ce4e0f405a5a269f
OsamaRafiq <99024906+OsamaRafiq@users.noreply.github.com>
2024-02-13 01:10:48 +0500

    Add files via upload

---
+++ JS_Assignment#1/app.js
@@ -0,0 +1,34 @@
+// Task#1
+console.log("Task 1 ");
+
+var a=+prompt("Enter First Number:");
+var b=+prompt("Enter Second Number:");
+
+var c=a+b;
+
+alert("Sum Of "+a+" and "+b+" is "+(a+b))
+
+console.log("Sum Of "+a+" and "+b+" is "+c);
+
+
+//Task #2
+console.log("Task 2 ");
+var firstName=prompt("Enter First Your Name:");
+var lastName=prompt("Enter Your Last Name:");
+
+alert("Your Full Name Is : "+firstName+" "+lastName);
+
+console.log("Your Full Name Is : "+firstName+" "+lastName);
+
+
+
+//Task#3
+console.log("Task 3");
+
+var isTrue;
+isTrue = true;
+
+console.log("The value of isTrue is: " + isTrue);
+
+
+
